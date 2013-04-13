f97b816ae8b2c94f95215b54f653f209e4f450e6
Daniel Lo Nigro <daniel@dan.cx>
2013-04-14 02:02:43 +1000

    Nicer JSON beautification

--- site/public/js/account.js
+++ site/public/js/account.js
@@ -16,8 +16,44 @@
 	});
 	
 	window.exampleCallback = function(data) {
-		$('#jsonData').html(JSON.stringify(data).replace(/,/g, ',<br />'));
-	}	
-})(window.jQuery);
+		// Very simple JSON beautification :-)
+		var rawJson = JSON.stringify(data),
+			beautifulJson = '',
+			currentLevel = 0,
+			SPACE_SIZE = 4;
+		
+		function addLineBreak() {
+			var spacesToAdd = SPACE_SIZE * (currentLevel - 1);
+			beautifulJson += '<br />';
+			while (spacesToAdd-- > 0) {
+				beautifulJson += ' ';
+			}
+		}
+		
+		for (var i = 0, length = rawJson.length; i < length; i++) {
+			var character = rawJson.charAt(i);
+			beautifulJson += character;
+			
+			switch (character) {
+				case '{':
+					currentLevel++;
+					addLineBreak();
+					break;
+				case '}':
+					// Remove '}' already added since it needs to go to the next line
+					beautifulJson = beautifulJson.slice(0, -1);
+					currentLevel--;
+					addLineBreak();
+					beautifulJson += '}';
+					break;
+				case ',':
+					addLineBreak();
+					break;
+			}
+		}
+		
+		$('#jsonData').html(beautifulJson);
+	};	
+}(window.jQuery));
 
 
